9a35a1284018ff0075a9c73ad5f9f5932120cf52
Dave Konopka <dave.konopka@gmail.com>
2016-04-07 14:16:36 -0400

    Add Circleci docker_lxc rspec support

--- tests/spec/spec_helper.rb
+++ tests/spec/spec_helper.rb
@@ -3,5 +3,10 @@ require 'docker'
 
 container_id = ENV['CONTAINER_ID']
 
-set :backend, :docker
+set :backend, if ENV['CIRCLECI']
+  :docker_lxc
+else
+  :docker
+end
+
 set :docker_container, container_id
